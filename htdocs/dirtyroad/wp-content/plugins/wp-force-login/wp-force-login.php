2e3db9136ca76e3097c66c126c233eea461863b2
Luis Mandel <luis.mandel@nttdata.com>
2022-01-21 00:13:22 +0100

    version 5.6.2 installed. Update to 5.6.3

--- htdocs/dirtyroad/wp-content/plugins/wp-force-login/wp-force-login.php
+++ htdocs/dirtyroad/wp-content/plugins/wp-force-login/wp-force-login.php
@@ -3,11 +3,13 @@
 Plugin Name: Force Login
 Plugin URI: https://wordpress.org/plugins/wp-force-login/
 Description: Easily hide your WordPress site from public viewing by requiring visitors to log in first. Activate to turn on.
-Version: 5.6.2
+Version: 5.6.3
 Author: Kevin Vess
 Author URI: http://vess.me/
+
 Text Domain: wp-force-login
 Domain Path: /languages
+
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
@@ -29,7 +31,7 @@ function v_forcelogin() {
 	$url = $schema . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 	// Bail if visiting the login URL. Fix for custom login URLs
-	if ( wp_login_url() === preg_replace( '/\?.*/', '', $url ) ) {
+	if ( preg_replace( '/\?.*/', '', wp_login_url() ) === preg_replace( '/\?.*/', '', $url ) ) {
 		return;
 	}
 
@@ -104,33 +106,3 @@ function v_forcelogin_load_textdomain() {
 	load_plugin_textdomain( 'wp-force-login', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
 add_action( 'plugins_loaded', 'v_forcelogin_load_textdomain' );
-
-/**
- * Bypass Force Login to allow for exceptions.
- *
- * @param bool $bypass Whether to disable Force Login. Default false.
- * @param string $visited_url The visited URL.
- * @return bool
- */
-function my_forcelogin_bypass( $bypass, $visited_url ) {
-  // Allow all single posts
-  if ( is_single() ) {
-    $bypass = true;
-  }
-
-  // Allow these absolute URLs
-  $allowed = array(
-    home_url( '/register/' ),
-    home_url( '/about/' ),
-	home_url( '/faq/' ),
-	home_url( '/impressum/' ),
-	home_url( '/photos/' ),
-	home_url( '/terms_and_conditions/' ),
-  );
-  if ( ! $bypass ) {
-    $bypass = in_array( $visited_url, $allowed );
-  }
-
-  return $bypass;
-}
-add_filter( 'v_forcelogin_bypass', 'my_forcelogin_bypass', 10, 2 );
